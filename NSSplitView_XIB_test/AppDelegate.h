a69ca18585c55629db8e6f8173242b717dbe3ff1
Gregory Casamento <greg.casamento@gmail.com>
2023-08-27 10:44:00 -0400

    Initial Commit

---
+++ NSSplitView_XIB_test/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  NSSplitView_XIB_test
+//
+//  Created by Gregory John Casamento on 8/27/23.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ NSSplitView_XIB_test/main.m
@@ -0,0 +1,15 @@
+//
+//  main.m
+//  NSSplitView_XIB_test
+//
+//  Created by Gregory John Casamento on 8/27/23.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // Setup code that might create autoreleased objects goes here.
+    }
+    return NSApplicationMain(argc, argv);
+}
